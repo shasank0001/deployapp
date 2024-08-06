814e874553261a8d910453f4ea024f44ea14a6ae
sasank varma <shasank12344@gmail.com>
2024-08-06 10:07:18 +0530

    Added my project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Header } from './componets/Header';
+import { Body } from './componets/bodey';
+function App() {
+  return (
+    <div>
+     <Header></Header>
+     <Body></Body>
+    </div>
+  )
+}
+
+export default App
---
+++ src/componets/bodey.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Body(){
+
+    return (
+        <div>
+            this some 
+        </div>
+    );
+}
